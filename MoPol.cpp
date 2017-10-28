41c746d647134c2d7958c0deab7a5ed46546cace
16207220 <2027620665@qq.com>
2017-10-28 15:35:56 +0800

    Mopol

---
+++ MoPol.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<conio.h>
+typedef struct Item {
+	double coef; 					// 系数 
+	int expn; 						//指数 
+	Item *next;
+}Item;
+
+
+
+
+int main() {
+	return 0;
+}
